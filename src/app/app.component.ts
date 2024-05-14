97b1e1d41f72cec7cc2388e0a65f2067949b90cb
ejwoods <ewoods08@gmail.com>
2024-05-14 16:31:18 -0400

    add WordInput component to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { WordInputComponent } from './word-input/word-input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    MatToolbarModule
+    MatToolbarModule,
+    WordInputComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
