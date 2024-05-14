b6d80d408badbf7cb638afcdde0462a6e15d307e
ejwoods <ewoods08@gmail.com>
2024-05-14 16:24:11 -0400

    create WordInput component

---
+++ src/app/word-input/word-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-word-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './word-input.component.html',
+  styleUrl: './word-input.component.css'
+})
+export class WordInputComponent {
+
+}
