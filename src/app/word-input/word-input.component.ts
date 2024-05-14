d49544cec8f6a972ca6ee3ac63e8c0560d83c103
ejwoods <ewoods08@gmail.com>
2024-05-14 16:59:07 -0400

    update WordInput to use ng mat components

--- src/app/word-input/word-input.component.ts
+++ src/app/word-input/word-input.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input'; 
 
 @Component({
   selector: 'app-word-input',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule],
   templateUrl: './word-input.component.html',
   styleUrl: './word-input.component.css'
 })
